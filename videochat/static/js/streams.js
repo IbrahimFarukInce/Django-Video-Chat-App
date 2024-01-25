999ef75a4da45591590ab4e4744d0250f90a3a57
ibrahim ince <ibrahimfarukince@gmail.com>
2024-01-25 03:33:58 +0300

    video chat app

---
+++ videochat/static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = 'e7eeff558045424890ad1e38fc511246'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYPhj6nym/DqDyk1n39iryzRX7T2b+CSYd7ePebZmxXfbAhkFhlTz1NS0NFNTCwMTUxMjEwtLg8QUw1Rji7RkU0NDIxOzm8s2pjYEMjIEbGZnYIRCEJ+FITcxM4+BAQAmhR6a'
+
+console.log('stream.js connected')
